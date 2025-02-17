fc68481e9a24d66c2c9bb661a40a5ababfe4ed40
oscaryen <mapd24th3ee@gmail.com>
2025-02-17 11:01:32 +0800

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Oscar Yen on 2025/2/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppTests.swift
+//  TestAppTests
+//
+//  Created by Oscar Yen on 2025/2/17.
+//
+
+import Testing
+@testable import TestApp
+
+struct TestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
